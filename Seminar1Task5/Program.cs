0e1e20cc38b5cbe9ed4c87ee3260f36827f30f9c
Alexander Kostylev <successful08@mail.ru>
2023-09-05 00:24:22 +0500

    solution sem1 task 5 fix

--- Seminar1Task5/Program.cs
+++ Seminar1Task5/Program.cs
@@ -3,9 +3,9 @@
 // а на выходе показывает все целые числа в промежутке от -N до N.
 
 Console.WriteLine("Enter number");
-int numN=int.Parse(Console.ReadLine()??"0");
+int numN = int.Parse(Console.ReadLine() ?? "0");
 
-for (int i=-1*numN; i>=numN; i++)
+for (int i = -1 * numN; i <= numN; i++)
 {
     Console.Write(i);
     Console.Write(", ");

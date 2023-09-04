ed3db8fe6592500119fb217438e57e4a46426268
Alexander Kostylev <successful08@mail.ru>
2023-09-04 23:48:25 +0500

    solution from seminar1 task 0,1,2,3,5

---
+++ Seminar1Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №3
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+
+// запрос дня недели от системы
+// string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum));
+
+
+Console.WriteLine("Enter day number");
+
+int day=int.Parse(Console.ReadLine()??"0");
---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №5
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Enter number");
+int numN=int.Parse(Console.ReadLine()??"0");
+
+for (int i=-1*numN; i>=numN; i++)
+{
+    Console.Write(i);
+    Console.Write(", ");
+}

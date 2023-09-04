0801a0a668d08406a24ef04401d7f57502bbf11f
Alexander Kostylev <successful08@mail.ru>
2023-09-04 23:51:06 +0500

    seminar 1 task 1

---
+++ Seminar1TAsk1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Введите два числа. Проверить является ли первое число квадратом второго.
+Console.WriteLine("Введите первое число: ");
+int firstNum=int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите второе число: ");
+int secondNum=int.Parse(Console.ReadLine()??"0");
+
+if(firstNum==Math.Pow(secondNum,2))
+{
+    Console.WriteLine("Число два является квадратом первого числа");
+}
+else
+{
+    Console.WriteLine("Число два неявляется квадратом первого числа");
+}

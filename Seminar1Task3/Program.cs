5c1613b104d5c5a32314d549ea316313ae129123
Alexander Kostylev <successful08@mail.ru>
2023-09-05 00:44:50 +0500

    sem1 fix task3

--- Seminar1Task3/Program.cs
+++ Seminar1Task3/Program.cs
@@ -9,3 +9,14 @@
 Console.WriteLine("Enter day number");
 
 int day=int.Parse(Console.ReadLine()??"0");
+
+string [] dayOfWeek={"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+
+if(day>=1&&day<8)
+    {
+        Console.WriteLine(dayOfWeek[day-1]);
+    }
+else
+    {
+        Console.WriteLine("День введе не верно, введите число от 1 до 7");
+    }   

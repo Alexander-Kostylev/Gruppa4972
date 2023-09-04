69c5fc5ddc59f5febc6abdd3e3d038c90183a95d
Alexander Kostylev <successful08@mail.ru>
2023-09-05 00:01:53 +0500

    solution from sem1 task 7

---
+++ Seminar1task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №7
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Enter the number: ");
+int num=int.Parse(Console.ReadLine()??"0");
+
+if(num>99&&num<1000)
+{
+    int lastNum=num%10;
+    Console.WriteLine("Последнее число "+num+" это "+lastNum);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное")
+}

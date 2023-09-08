a06d8e2dda37b85111b61ceb2fbb9b061cf8270a
Alexander Kostylev <successful08@mail.ru>
2023-09-09 01:00:00 +0500

    Add homework sem2task 10, 15

--- Seminar2Task10/Program.cs
+++ Seminar2Task10/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Домашнее задание!
+// Написать программу, 
+// которая на вход принимает трехзначеное число 
+// и на выходе показывает вторую цифру этого числа. 
+
+int getValue()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int val = int.Parse(Console.ReadLine() ?? "0");
+    return val;
+}
+
+while (true)
+{
+    int num = getValue();
+    char[] numInArray = num.ToString().ToCharArray();
+    if (numInArray.Length == 3)
+    {
+        Console.WriteLine(numInArray[1]);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не трехзначное!");
+    }
+}
+
+

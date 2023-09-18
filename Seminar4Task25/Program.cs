c9f23e44a0750cdd108851f207a2ad1561e3b41b
Alexander Kostylev <successful08@mail.ru>
2023-09-19 01:20:48 +0500

    Seminar 4 homework add Sem4Task30_Dop

--- Seminar4Task25/Program.cs
+++ Seminar4Task25/Program.cs
@@ -1,1 +1,114 @@
-﻿
+﻿//============================================================
+// Основное задание.
+// Написать цикл, который 
+// принимает на вход два числа (А и В) 
+// и возводит число А в натуральную степень В.
+//============================================================
+
+/*
+// Получение данных от пользователя 
+int readData(string message)
+{
+    Console.Write(message);
+    int inputData = int.Parse(Console.ReadLine() ?? "0");
+    return inputData;
+}
+
+// Возведение в степень
+long raiseToADegree(int num, int degree)
+{
+    int tempDegree = 1;
+    int result = 1;
+    while (tempDegree <= degree)
+    {
+        result *= num;
+        tempDegree++;
+    }
+    return result;
+}
+
+// Вывод результата
+void outConsole(string mes)
+{
+    Console.WriteLine(mes);
+}
+
+int number = readData("Введите число: ");
+int valueOfDegree = readData("Введите степень: ");
+
+long result = raiseToADegree(number, valueOfDegree);
+
+outConsole($"{number}^{valueOfDegree}={result}");
+
+*/
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// * Дополнительное задание.
+// Программа калькулятор с операциями 
+// +,-,/,*, возведение в степень
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Получение данных от пользователя
+using System.Collections;
+
+int readData(string message)
+{
+    Console.Write(message);
+    int inputData = int.Parse(Console.ReadLine() ?? "0");
+    return inputData;
+}
+
+// Вывод результата
+void outConsole(string mes)
+{
+    Console.WriteLine(mes);
+}
+
+// выбор операции над числами
+char outSimvol()
+{
+
+    while (true)
+    {
+        Console.WriteLine("Выберите одно из следующих действий: +, -, *, /, ^");
+        char operation = char.Parse(Console.ReadLine() ?? "0");
+        if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^')
+            return operation;
+        Console.WriteLine($"{operation} - данного действия не предусмотренно.");
+    }
+
+}
+
+double calculator(int num1, int num2, char oper)
+{
+    double res = 1;
+    switch (oper)
+    {
+        case ('+'):
+            res = num1 + num2;
+            break;
+        case ('-'):
+            res = num1 - num2;
+            break;
+        case ('*'):
+            res = num1 * num2;
+            break;
+        case ('/'):
+            res = (double)num1 / num2;
+            break;
+        case ('^'):
+            res = Math.Pow(num1, num2);
+            break;
+
+    }
+    return res;
+
+}
+
+int number1 = readData("Введите 1-е число: ");
+int number2 = readData("Введите 2-е число: ");
+char op = outSimvol();
+
+double result = calculator(number1, number2, op);
+
+outConsole($"{number1} {op} {number2} = {result}");
--- Seminar4Task27/Program.cs
+++ Seminar4Task27/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿//============================================================
+// Основное задание.
+// Написать программу, которая на вход принимает число 
+// и выдает сумму всех чисел в числе.
+//============================================================
+
+/*
+// Получение данных от пользователя 
+long readData(string message)
+{
+    Console.Write(message);
+    long inputData = long.Parse(Console.ReadLine() ?? "0");
+    return inputData;
+}
+
+// Вывод результата
+void outConsole(string mes)
+{
+    Console.WriteLine(mes);
+}
+
+long sumOfNumber(long num)
+{
+    long sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+long inputNumber = readData("Введите число: ");
+long resultSum = sumOfNumber(inputNumber);
+outConsole($"Сумма всех чисел в числе {inputNumber} = {resultSum}");
+
+*/
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// * Дополнительное задание.
+// Оценка времени работы алгоритма через вещественые числа и строки
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+char[] chars=(Console.ReadLine()??"0").ToCharArray();
+double sum=0;
+foreach(char op in chars)
+{
+    sum+=Char.GetNumericValue(op);
+}
+System.Console.WriteLine(sum);

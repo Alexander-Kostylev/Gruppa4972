e28da59ae2184c03ea08f3633b72c98019c2e213
Alexander Kostylev <successful08@mail.ru>
2023-09-26 00:50:29 +0500

    Seminar 6 Tast 41 and 43 womehork done

---
+++ Seminar6Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿//=====================================================================
+//                   Домашнее задание
+//          Пользователь вводит с клавиатуры M чисел.
+//    Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//=====================================================================
+
+//  Ввод значений
+string ReadData(string msg)
+{
+    Console.Write(msg);
+    string str = Console.ReadLine() ?? "0";
+    return str;
+}
+
+//  Преобразование строки -> число
+int[] StringToIntegerArray(string str)
+{
+    string[] numbes = str.Split(',');
+    int[] value = new int[numbes.Length];
+    for (int i = 0; i < numbes.Length; i++)
+    {
+        value[i] = int.Parse(numbes[i]);
+    }
+    return value;
+}
+
+//  Нахождение количества чисел >0
+int CheckNumberGreaterZero(int[] arr)
+{
+    int count = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[j] > 0) count++;
+    }
+    return count;
+}
+//  Вывод результата    
+void PrintData(int result)
+{
+    Console.WriteLine($"Ввели {result} чисел(-а) больше 0");
+}
+
+string enterValue = ReadData("Введите числа через запятую: ");
+int[] valueInteger = StringToIntegerArray(enterValue);
+int numbGreaterThanZero = CheckNumberGreaterZero(valueInteger);
+PrintData(numbGreaterThanZero);
---
+++ Seminar6Task43/Program.cs
@@ -0,0 +1,53 @@
+﻿//===============================================================================
+//                             Домашнее задание 
+//         Напишите программу, которая найдёт точку пересечения двух прямых, 
+//              заданных уравнениями y = k1 * x + b1, y = k2 * x + b2. 
+//                  Значения b1, k1, b2 и k2 задаются пользователем.
+//===============================================================================
+
+//  Ввод значений
+string[] ReadData(string msg)
+{
+    Console.Write(msg);
+
+    string koord=Console.ReadLine() ?? "0";
+    string[] strNum=koord.Split(',');
+
+    return strNum;
+}
+
+//  Преобразование строки->число для математич. операций
+int[] StringToIntegerArray(string[] str)
+{
+    int[] lineValue=new int[str.Length];
+    lineValue[0] = Convert.ToInt32(str[0]);
+    lineValue[1] = Convert.ToInt32(str[1]);
+
+    return lineValue;
+}
+
+//  Расчет точки пересечения прямых
+(int, int) IntersectionPoint(int[] lineFerst, int[] lineSecond)
+{
+    int pointX = (lineSecond[1] - lineFerst[1]) / (lineFerst[0] - lineSecond[0]);
+    int pointY = lineSecond[0] * (pointX)+lineSecond[1];
+    return(pointX, pointY);
+}
+
+//  Вывод результата на экран
+void PrintData(int x, int y)
+{
+    Console.WriteLine($"Точка пересечения линий ({x}, {y})");
+}
+
+
+
+string[] enterLineFirst = ReadData("Введите координаты k и b для 1 линии: ");
+string[] enterLineSecond = ReadData("Введите координаты k и b для 2 линии: ");
+
+int[] lineFirst = StringToIntegerArray(enterLineFirst);
+int[] lineSecond = StringToIntegerArray(enterLineSecond);
+
+(int x, int y) point= IntersectionPoint(lineFirst, lineSecond);
+
+PrintData(point.x, point.y);

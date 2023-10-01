1480484406de9c323b273aabc64bbfcbe725922e
Alexander Kostylev <successful08@mail.ru>
2023-10-02 00:58:39 +0500

    Add sem 8 homework task 60 and 62

---
+++ Seminar8Task60/Program.cs
@@ -0,0 +1,62 @@
+﻿// ==============================================================================================================
+//                                        Задание 60
+//                  Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//      Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//                                  Массив размером 2 x 2 x 2.
+// ==============================================================================================================
+
+//проверка на совпадение числа с уже используемыми в массиве
+int CheckRandomValue(int value, int[] arr)
+{
+    return Array.IndexOf(arr, value);
+}
+
+// создание и заполнение 3-х мерного массива неповторяющимися числами
+int[,,] Create3DArray()
+{
+    int[,,] arr3D = new int[2, 2, 2];
+    int[] tempArray = new int[arr3D.GetLength(0) * arr3D.GetLength(1) * arr3D.GetLength(2)];
+    int count = 0;
+    
+    for (int i = 0; i < arr3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr3D.GetLength(2); k++)
+            {
+                do
+                {
+                    arr3D[i, j, k] = new Random().Next(10, 100);                                                     
+                }
+                while (CheckRandomValue(arr3D[i, j, k], tempArray) != -1);
+                tempArray[count] = arr3D[i, j, k];
+                count++;
+            }
+        }
+    }
+    return arr3D;
+}
+
+// Печать 3-х мерного массива
+void Print3DArray(int[,,] arr3D)
+{
+    for (int i = 0; i < arr3D.GetLength(0); i++)              //слой
+    {
+        for (int j = 0; j < arr3D.GetLength(1); j++)          //строка
+        {
+            for (int k = 0; k < arr3D.GetLength(2); k++)      //столбец
+            {
+                Console.Write($"{arr3D[i, j, k]} ({i}, {j}, {k})\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+//создание 3-х мерного массива и заполнение неповторяющиемеся числами
+int[,,] arr3D = Create3DArray();
+
+// печать 3-х мерного массива
+Print3DArray(arr3D);
+
---
+++ Seminar8Task62/Program.cs
@@ -0,0 +1,82 @@
+﻿//============================================================================
+//                          Задача 62. 
+//       Написать программу, которая заполнит спирально массив.
+//           Разбор решения. РЕШЕНИЕ ВЗЯТО У ТАТЬЯНЫ!!!
+//============================================================================
+
+
+// Записываем данные из консоли в переменный
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Заполняем двумерный массив спирально
+int[,] FillSpiralArray(int rows, int columns)//, int topBorder, int downBorder
+{
+    int[,] array = new int[rows, columns];
+    int value = 1;
+    
+    // Границы массива
+    int left = 0, right = columns - 1, top = 0, bottom = rows - 1; 
+    
+    // Пока не заполнены все ячейки
+    while (value <= rows * columns)
+    {
+        // Заполняем верхнюю строку слева направо
+        for (int i = left; i <= right && value <= rows * columns; i++)
+        {
+            array[top, i] = value;
+            value++;
+        }
+        top++;
+
+        // Заполняем правый столбец сверху вниз
+        for (int i = top; i <= bottom && value <= rows * columns; i++)
+        {
+            array[i, right] = value;
+            value++;
+        }
+        right--;
+
+        // Заполняем нижнюю строку справа налево
+        for (int i = right; i >= left && value <= rows * columns; i--)
+        {
+            array[bottom, i] = value;
+            value++;
+        }
+        bottom--;
+
+        // Заполняем левый столбец снизу вверх
+        for (int i = bottom; i >= top && value <= rows * columns; i--)
+        {
+            array[i, left] = value;
+            value++;
+        }
+        left++;
+    }
+    
+    return array;
+}
+
+//печать 2Д массива
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rowArr=ReadData($"Введите количество строк в масииве: ");
+int columnArr=ReadData($"Введите количество столбцов в масииве: ");
+
+int[,] spiralArray=FillSpiralArray(rowArr,columnArr);
+Console.WriteLine();
+Print2DArray(spiralArray);
